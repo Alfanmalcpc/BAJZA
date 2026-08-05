b4032e9db2542b5ba733af3dc529b58ae40c2e1c
Alfan <alfanbinkhoirul@gmail.com>
2026-08-05 21:44:24 +0700

    file

---
+++ firebase-multiplay.js
@@ -0,0 +1,16 @@
+// firebase-multiplay.js — Secondary Firebase Config for Photobooth Multiplayer
+
+const mpFirebaseConfig = {
+  apiKey: "AIzaSyDOco4URrTX8LEUHAyzBHnTh3qYSkSANWQ",
+  authDomain: "baja-multiplay-photobooth.firebaseapp.com",
+  databaseURL: "https://baja-multiplay-photobooth-default-rtdb.firebaseio.com",
+  projectId: "baja-multiplay-photobooth",
+  storageBucket: "baja-multiplay-photobooth.firebasestorage.app",
+  messagingSenderId: "24017302998",
+  appId: "1:24017302998:web:beeae381d6160c6511e077",
+  measurementId: "G-M9PPZEKFB3"
+};
+
+// Initialize Secondary Firebase App for Multiplayer Database
+const mpApp = firebase.initializeApp(mpFirebaseConfig, "MultiplayerApp");
+const mpDb = mpApp.database();
