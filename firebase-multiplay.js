355f1e437adab111cd43296a93a5a4c15d0ed72e
Alfan <alfanbinkhoirul@gmail.com>
2026-08-06 08:42:56 +0700

    file

--- firebase-multiplay.js
+++ firebase-multiplay.js
@@ -3,7 +3,7 @@
 const mpFirebaseConfig = {
   apiKey: "AIzaSyDOco4URrTX8LEUHAyzBHnTh3qYSkSANWQ",
   authDomain: "baja-multiplay-photobooth.firebaseapp.com",
-  databaseURL: "https://baja-multiplay-photobooth-default-rtdb.firebaseio.com",
+  databaseURL: "https://baja-multiplay-photobooth-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "baja-multiplay-photobooth",
   storageBucket: "baja-multiplay-photobooth.firebasestorage.app",
   messagingSenderId: "24017302998",
